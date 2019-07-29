a45a24eb89afa45b801a0ea8644df4443e1a9da6
Mico Piira <mico.piira@gmail.com>
2019-07-29 16:01:10 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const hue = require('hue-api');
+
+
+const port = process.env.PORT || 3000;
+const clientId = process.env.HUE_CLIENT_ID;
+const clientSecret = process.env_HUE_CLIENT_SECRET;
+const frontendUrl = process.env.FRONTEND_URL;
+
+app.get('/', (req, res) => {
+	res.send(hue.oauth2.getOauthLink(clientId));
+});
+
+app.get('/callback', (req, res) => {
+	hue.oauth2.getToken(clientId, clientSecret, req.query.code)
+		.then(json => res.redirect(frontendUrl + '?access_token=' + json.access_token));
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
