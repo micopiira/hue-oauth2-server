daa7fa1a4bc5aebd68359ebafbd89adf1fb6748d
Mico Piira <mico.piira@gmail.com>
2019-07-29 23:28:59 +0300

    updates

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const express = require('express');
 const app = express();
-const hue = require('hue-api');
+const hue = require('hue-api')();
+const cors = require('cors');
 
+require('dotenv').config();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 const clientId = process.env.HUE_CLIENT_ID;
 const clientSecret = process.env.HUE_CLIENT_SECRET;
-const frontendUrl = process.env.FRONTEND_URL;
+const frontendUrl = process.env.FRONTEND_URL || 'http://localhost:3000/';
+
+app.use(cors());
 
 app.get('/', (req, res) => {
-	res.send(hue.oauth2.getOauthLink(clientId));
+	res.redirect(hue.oauth2.getOauthLink(clientId));
+});
+
+app.get('/v2/:path', (req, res) => {
+	console.log({path: req.params.path, accessToken: req.header('Authorization').split(' ')[1]});
+	res.header('Access-Control-Allow-Origin', '*');
+	hue.getJson(req.header('Authorization').split(' ')[1], '/v2/' + req.params.path).then(json => res.json(json));
 });
 
 app.get('/callback', (req, res) => {
